8fa8019bd9e07f0adac54174aca7ef2b54e0a695
Emma McLarnon <emma.mclarnon@hmrc.gsi.gov.uk>
2018-11-25 20:56:11 +0000

    Added icons for restaurants

--- javascript/leaflet.js
+++ javascript/leaflet.js
@@ -1,9 +1,19 @@
-var map = L.map('mapid').setView([51.505, -0.09], 13);
+var map = L.map('mapid').setView([53.815101 , 	-1.562214], 12);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    L.tileLayer('https://tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=c4fdde49b9a14b189c0fc231906018e5', {
+	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	apikey: 'c4fdde49b9a14b189c0fc231906018e5'}).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+var aubIcon = L.icon({
+    iconUrl: 'aub.png',
+    //shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [38, 95], // size of the icon
+    //shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    //hadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+L.marker([53.815101 , 	-1.562214] ,{icon: aubIcon, title: 'LS6 Cafe'}).addTo(map).bindPopup("LS6 Cafe");
+L.marker([53.828075 , 	-1.537715],{title: 'Seven Arts Cafe',icon: aubIcon}).addTo(map).bindPopup("Seven Arts Cafe");
+//L.marker([53.815101 , 	-1.562214], {icon: aubIcon}).addTo(map).bindPopup("LS6 Cafe");
